4420cdec8425c9f08e621fb06c41d43786ed0d84
Murali Manohar Varma <67296473+mmv-dev@users.noreply.github.com>
2021-07-18 15:39:09 +0530

    Self-Test fix

--- ClointFusion/ClointFusion.py
+++ ClointFusion/ClointFusion.py
@@ -3208,6 +3208,80 @@ def isNaN(value):
     except:
         return False
 
+def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None, startcol=None,
+    truncate_sheet=False, resizeColumns=True, na_rep = 'NA', **to_excel_kwargs):
+    
+    from string import ascii_uppercase
+    from openpyxl.utils import get_column_letter
+    
+    # ignore [engine] parameter if it was passed
+    if 'engine' in to_excel_kwargs:
+        to_excel_kwargs.pop('engine')
+
+    try:
+        f = open(filename)
+        # Do something with the file
+    except IOError:
+        # print("File not accessible")
+        wb = Workbook()
+        ws = wb.active
+        ws.title = sheet_name
+        wb.save(filename)
+
+    writer = pd.ExcelWriter(filename, engine='openpyxl', mode='a')
+
+    try:
+        # try to open an existing workbook
+        writer.book = load_workbook(filename)
+
+        # get the last row in the existing Excel sheet
+        # if it was not specified explicitly
+        if startrow is None and sheet_name in writer.book.sheetnames:
+            startrow = writer.book[sheet_name].max_row
+
+        # truncate sheet
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            # index of [sheet_name] sheet
+            idx = writer.book.sheetnames.index(sheet_name)
+            # remove [sheet_name]
+            writer.book.remove(writer.book.worksheets[idx])
+            # create an empty sheet [sheet_name] using old index
+            writer.book.create_sheet(sheet_name, idx)
+
+        # copy existing sheets
+        writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
+    except FileNotFoundError:
+        # file does not exist yet, we will create it
+        pass
+
+    if startrow is None:
+        # startrow = -1
+        startrow = 0
+
+    if startcol is None:
+        startcol = 0
+
+    # write out the new sheet
+    df.to_excel(writer, sheet_name, startrow=startrow, startcol=startcol, na_rep=na_rep, **to_excel_kwargs)
+
+    if resizeColumns:
+
+        ws = writer.book[sheet_name]
+
+        def auto_format_cell_width(ws):
+            for letter in range(1,ws.max_column):
+                maximum_value = 0
+                for cell in ws[get_column_letter(letter)]:
+                    val_to_check = len(str(cell.value))
+                    if val_to_check > maximum_value:
+                        maximum_value = val_to_check
+                ws.column_dimensions[get_column_letter(letter)].width = maximum_value + 2
+
+        auto_format_cell_width(ws)
+
+    # save the workbook
+    writer.save()
+
 # ---------  Excel Functions Ends --------- 
 
 
