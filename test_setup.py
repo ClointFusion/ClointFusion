d5d7cc200d6804380fd6fdd373fe7de91cf91be8
Murali Manohar Varma <67296473+mmv-dev@users.noreply.github.com>
2021-11-07 04:59:07 +0530

    1.1.2 PRE

--- test_setup.py
+++ test_setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(), 
     include_package_data=True,
     zip_safe=False,
-    version='1.2.9', # Test Versrion is more than Official Version
+    version='1.3.0', # Test Versrion is more than Official Version
     description="Python based Automation (RPA) Platform",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -83,3 +83,4 @@ setup(
 # 1.2.7
 # 1.2.8
 # 1.2.9
+# 1.3.0
